67aeb15439df3d0fd775c5edf69661399f6afc1b
Cliff Delsoin <cdelsoin@gmail.com>
2017-12-31 00:12:01 -0500

    test - sort seats by class, row, then seat

--- App.js
+++ App.js
@@ -6,11 +6,47 @@ export default class App extends React.Component {
   render() {
 
     const seatData = Seats
+    const firstClass = []
+    const businessClass = []
+    const economyClass = []
+
+    seatData.forEach(function(seat){
+      switch (seat.class) {
+        case 'First':
+            firstClass.push(seat)
+          break;
+        case 'Business':
+            businessClass.push(seat)
+          break;
+        case 'Economy':
+            economyClass.push(seat)
+          break;
+        default:
+
+      }
+    })
+
+    firstClass.sort(function (a, b) {
+      if (a.row > b.row) {
+          return 1;
+      } else if (a.row < b.row) {
+          return -1;
+      }
+
+      // Else go to the 2nd item
+      if (a.seat < b.seat) {
+          return -1;
+      } else if (a.seat > b.seat) {
+          return 1
+      } else {
+          return 0;
+      }
+    })
 
     return (
       <ScrollView style={styles.container}>
 
-        {seatData.map(function(seat, index){
+        {firstClass.map(function(seat, index){
           return <Text key="index"> {seat.class}, {seat.row}, {seat.seat} </Text>
         })}
       </ScrollView>
