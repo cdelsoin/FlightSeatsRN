c731c431c44fd64a07f65b513d5afc414c7a898a
Cliff Delsoin <cdelsoin@gmail.com>
2018-01-01 08:18:09 -0500

    create seat legend component

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ class Seat extends React.Component {
           this.props.seat.selected && styles.selectedSeat,
           this.props.seat.occupied && styles.occupiedSeat,
         ]}>
-        <Text style={{color: '#ccc'}}>{this.props.seat.row}{this.props.seat.seat}</Text>
+        <Text style={[{color: '#ccc'}, this.props.seat.premium && {color: '#000'}]}>{this.props.seat.row}{this.props.seat.seat}</Text>
       </TouchableHighlight>
     )
   }
@@ -55,7 +55,7 @@ class AisleSeat extends React.Component {
           this.props.seat.selected && styles.selectedSeat,
           this.props.seat.occupied && styles.occupiedSeat,
         ]}>
-        <Text style={{color: '#ccc'}}>{this.props.seat.row}{this.props.seat.seat}</Text>
+        <Text style={[{color: '#ccc'}, this.props.seat.premium && {color: '#000'}]}>{this.props.seat.row}{this.props.seat.seat}</Text>
       </TouchableHighlight>
     )
   }
@@ -86,6 +86,31 @@ class EconomyClassSection extends React.Component {
     return (
       <View style={styles.economyClassContainer}>
         {this.props.economyClassSeats}
+      </View>
+    )
+  }
+}
+
+class SeatLegend extends React.Component {
+  render(props){
+    return (
+      <View style={styles.seatLegendContainer}>
+        <View style={styles.seatDefinition}>
+          <View style={styles.legendSeatAvailable}></View>
+          <Text>Available</Text>
+        </View>
+        <View style={styles.seatDefinition}>
+          <View style={styles.legendSeatUnavailable}></View>
+          <Text>Unavailable</Text>
+        </View>
+        <View style={styles.seatDefinition}>
+          <View style={styles.legendSeatPremium}></View>
+          <Text>Premium</Text>
+        </View>
+        <View style={styles.seatDefinition}>
+          <View style={styles.legendSeatSelected}></View>
+          <Text>Selected</Text>
+        </View>
       </View>
     )
   }
@@ -202,8 +227,11 @@ export default class App extends React.Component {
           <BusinessClassSection businessClassSeats={businessClassSeats}/>
           <EconomyClassSection economyClassSeats={economyClassSeats}/>
         </ScrollView>
-        <View style={styles.zView}>
-          <Text>Hello</Text>
+        <View style={styles.legendContentContainer}>
+          <SeatLegend/>
+        </View>
+        <View style={styles.infoContentContainer}>
+          <Text>hi</Text>
         </View>
 
       </View>
@@ -216,9 +244,14 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     flexWrap: 'wrap',
   },
-  zView: {
+  legendContentContainer: {
+    backgroundColor: '#fff',
+    height: 30,
+    // marginTop:30
+  },
+  infoContentContainer: {
     backgroundColor: '#e93697',
-    height: 150,
+    height: 125,
   },
   contentContainer: {
     // flex: 1,
@@ -227,37 +260,50 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     // overflow: 'scroll'
   },
+
   firstClassContainer: {
-    // backgroundColor: '#6f257b',
+    backgroundColor: '#6f257b',
     flexDirection: 'row',
     flexWrap: 'wrap',
     marginTop: 40,
-    marginRight: 50,
-    marginLeft: 50,
+    marginRight: 10,
+    marginLeft: 10,
+    paddingTop: 10,
+    paddingRight: 40,
+    paddingBottom: 10,
+    paddingLeft: 40,
   },
   businessClassContainer: {
-    // backgroundColor: '#6f257b',
+    backgroundColor: '#6f257b',
     flexDirection: 'row',
     flexWrap: 'wrap',
-    marginTop: 15,
-    marginRight: 30,
-    marginLeft: 30,
+    marginTop: 20,
+    marginRight: 10,
+    marginLeft: 10,
+    paddingTop: 10,
+    paddingRight: 20,
+    paddingBottom: 10,
+    paddingLeft: 20,
   },
   economyClassContainer: {
-    // backgroundColor: '#6f257b',
+    backgroundColor: '#6f257b',
     flexDirection: 'row',
     flexWrap: 'wrap',
-    marginTop: 15,
-    marginRight: 5,
-    marginLeft: 5,
+    marginTop: 20,
+    marginRight: 10,
+    marginLeft: 10,
     marginBottom: 30,
+    paddingTop: 10,
+    paddingRight: 8,
+    paddingBottom: 10,
+    paddingLeft: 8,
   },
   seat: {
     alignItems: 'center',
     backgroundColor: '#3696e9',
     borderRadius: 25,
     borderColor: '#6f257b',
-    borderWidth: 2,
+    borderWidth: 1,
     height: 30,
     justifyContent: 'center',
     width: 30,
@@ -275,7 +321,7 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
   economyClassSeat: {
-    margin:5
+    margin:3
   },
   firstClassAisleSeat: {
     marginRight: 65
@@ -294,7 +340,57 @@ const styles = StyleSheet.create({
     opacity: 0.2
   },
   premiumSeat: {
-    backgroundColor: '#1463ff',
+    backgroundColor: '#dab537',
+  },
+  seatLegendContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between'
+  },
+  seatDefinition: {
+    // backgroundColor: '#6f257b',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginRight: 3,
+    marginLeft: 2,
+    marginTop: 5,
+    // padding: 5
+  },
+  legendSeatAvailable: {
+    backgroundColor: '#3696e9',
+    borderRadius: 25,
+    borderColor: '#fff',
+    borderWidth: 1,
+    height: 20,
+    width: 20,
+  },
+  legendSeatUnavailable: {
+    marginRight: 1,
+    backgroundColor: 'gray',
+    borderRadius: 25,
+    borderColor: '#fff',
+    borderWidth: 1,
+    height: 20,
+    opacity: 0.2,
+    width: 20,
+  },
+  legendSeatPremium: {
+    marginRight: 1,
+    backgroundColor: '#dab537',
+    borderRadius: 25,
+    borderColor: '#fff',
+    borderWidth: 1,
+    height: 20,
+    width: 20,
+  },
+  legendSeatSelected: {
+    marginRight: 1,
+    backgroundColor: '#14fff8',
+    borderRadius: 25,
+    borderColor: '#fff',
+    borderWidth: 1,
+    height: 20,
+    width: 20,
   }
 });
 
